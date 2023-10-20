ffeb843092cb7a031ad1a8a4fc3b39029cc2ef10
marlabharghavsai <147792275+marlabharghavsai@users.noreply.github.com>
2023-10-20 10:14:52 +0530

    #include<stdio.h> int main() {     int x,a,b,c;     scanf("%d%d%d",&x,&a,&b);     c=(a*1)+(b*2);     if(x<=c)     {         printf("Qualify");     }     else     {         printf("Not Qualify");     } }

---
+++ Contest:.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int x,a,b,c;
+    scanf("%d%d%d",&x,&a,&b);
+    c=(a*1)+(b*2);
+    if(x<=c)
+    {
+        printf("Qualify");
+    }
+    else
+    {
+        printf("Not Qualify");
+    }
+}

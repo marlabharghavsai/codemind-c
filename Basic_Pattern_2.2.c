94235f5d98f527c5ffbe49b94145a6eda89243c6
marlabharghavsai <147792275+marlabharghavsai@users.noreply.github.com>
2023-12-16 11:43:48 +0530

    #include<stdio.h> int main() {     int i,j,a;     scanf("%d",&a);     for(i=1;i<=a;i++)     {         for(j=1;j<=a;j++)         {             if(i>=j)             {             printf("%d ",i);             }         }         printf(" ");     } }

---
+++ Basic_Pattern_2.2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,a;
+    scanf("%d",&a);
+    for(i=1;i<=a;i++)
+    {
+        for(j=1;j<=a;j++)
+        {
+            if(i>=j)
+            {
+            printf("%d ",i);
+            }
+        }
+        printf("
+");
+    }
+}

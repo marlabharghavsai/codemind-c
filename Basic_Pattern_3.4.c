ec60750407c0c451993c6634a7bd5d1d415602fa
marlabharghavsai <147792275+marlabharghavsai@users.noreply.github.com>
2023-12-16 12:07:58 +0530

    #include<stdio.h> int main() {     int i,j,a;     scanf("%d",&a);     for(i=64+a;i>=65;i--)     {         for(j=64+a;j>=65;j--)         {             if(i>=j)             {             printf("%c ",i);             }         }         printf(" ");     } }

---
+++ Basic_Pattern_3.4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,a;
+    scanf("%d",&a);
+    for(i=64+a;i>=65;i--)
+    {
+        for(j=64+a;j>=65;j--)
+        {
+            if(i>=j)
+            {
+            printf("%c ",i);
+            }
+        }
+        printf("
+");
+    }
+}

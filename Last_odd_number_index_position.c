0b8c23ff44068c3d9e690c65bdc9fcf7da64471e
marlabharghavsai <147792275+marlabharghavsai@users.noreply.github.com>
2024-01-19 14:30:05 +0530

    #include<stdio.h> int main() {     int n,t;     scanf("%d",&n);     int a[n],i,k;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2==1)         {             t=i;         }     }     printf("%d",t); }

---
+++ Last_odd_number_index_position.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,t;
+    scanf("%d",&n);
+    int a[n],i,k;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2==1)
+        {
+            t=i;
+        }
+    }
+    printf("%d",t);
+}
